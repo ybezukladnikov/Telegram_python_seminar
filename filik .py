e988a53aa2456ca34152483f6885543d24d8f5c8
Yaroslav_Bezukladnikov <ybezukladnikov@gmail.com>
2022-10-02 14:55:47 +0300

    work telegram bot

--- filik .py
+++ filik .py
@@ -1,3 +1,4 @@
+from anecAPI import anecAPI
 from telegram import Update, Bot
 from telegram.ext import Updater, CommandHandler, Filters, MessageHandler, ConversationHandler
 from config import TOKEN
@@ -9,12 +10,19 @@ dispatcher = updater.dispatcher
 
 def start(update, context):
     arg = context.args
-    print(arg)
     if not arg:
         context.bot.send_message(update.effective_chat.id, "Привет")
     else:
         context.bot.send_message(update.effective_chat.id, f"{' '.join(arg)}")
 
+def del_abv(update, context):
+    arg = context.args
+    sub_string = 'абв'
+    text_new = ' '.join(filter(lambda x: sub_string not in x, arg))
+    if not arg:
+        context.bot.send_message(update.effective_chat.id, "Введите текст через пробел")
+    else:
+        context.bot.send_message(update.effective_chat.id, f"{text_new}")
 
 def info(update, context):
     context.bot.send_message(update.effective_chat.id,
@@ -33,7 +41,13 @@ def message(update, context):
 
 
 def unknown(update, context):
-    context.bot.send_message(update.effective_chat.id, f'Шо сказал, не пойму')
+    comand = update.message.text
+    joke='Нет такой команды'
+    if comand == '/modern': joke = anecAPI.modern_joke()
+    elif comand == '/sovet': joke = anecAPI.soviet_joke()
+    elif comand == '/random':joke = anecAPI.random_joke()
+    context.bot.send_message(update.effective_chat.id, joke)
+
 
 
 def give_word(update, context):
@@ -45,17 +59,19 @@ def give_word(update, context):
                 - Это всё, что вас удивляет? - с обидой говорит медведь.'''
         context.bot.send_message(update.effective_chat.id, joke)
         return joke
-    elif "пока" in word:
-        context.bot.send_message(update.effective_chat.id, 'Покачивай шляпой из бара')
+    elif "пика" in word:
+        context.bot.send_message(update.effective_chat.id, 'Твой пика тебе очень сильно любит и скучает!')
         return word
     context.bot.send_message(update.effective_chat.id, 'Вы, как всегда, правы, милорд')
 
 
+del_abv_handler = CommandHandler('del_abv', del_abv)
 start_handler = CommandHandler('start', start)
 info_handler = CommandHandler('info', info)
 message_handler = MessageHandler(Filters.text, give_word)
 unknown_handler = MessageHandler(Filters.command, unknown)  # /game
 
+dispatcher.add_handler(del_abv_handler)
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(info_handler)
 # dispatcher.add_handler(conv_handler)
